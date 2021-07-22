f746665f85f4f343b14ac7f0bb1bc08bb46cd490
johnmanjiro13 <juntama193@gmail.com>
2021-07-22 11:27:26 +0900

    restaurant

---
+++ restaurant/src/front_of_house.rs
@@ -0,0 +1,18 @@
+pub mod hosting;
+
+mod serving {
+  fn take_order() {}
+
+  fn serve_order() {}
+
+  mod back_of_house {
+    fn fix_incorrect_order() {
+      cook_order();
+      super::serve_order();
+    }
+
+    fn cook_order() {}
+  }
+
+  fn take_payment() {}
+}
